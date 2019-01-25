309c94ce8ba5066847db37a6167073523a8e088a
Arman Navarro <33739348+armannavarro@users.noreply.github.com>
2019-01-25 13:19:53 +0800

    Login Test
    Separate test script for Valid and Invalid Login

---
+++ Invalid Login.py
@@ -0,0 +1,59 @@
+import datetime
+import time
+import string
+import random
+from random import randint
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+class Full_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        #create a new chrome test session
+        #set the webdriver exec path
+        self.driver = webdriver.Chrome(executable_path='C:/Users/kenma/Downloads/chromedriver_win/chromedriver')
+        self.driver.implicitly_wait(2)
+
+        #navigate the web address
+        self.driver.get("https://test.id.overwatchid.com/login")
+        self.driver.implicitly_wait(2)
+
+        self.driver.maximize_window()
+
+
+    def test_1a_Log_in_with_Invalid_Credentials(self):
+        #Invalid username
+        self.driver.implicitly_wait(2)
+        self.user_name = self.driver.find_element_by_id("j_username")
+        self.user_name.send_keys("narman@test.com")
+        self.driver.implicitly_wait(2)
+        self.loginbtn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/form/div[4]/input')
+        self.loginbtn.click()
+        self.driver.implicitly_wait(2)
+        #Invalid Password
+        self.password = self.driver.find_element_by_id("j_pwd")
+        self.password.send_keys("Invalid")
+        #Login
+        self.login = self.driver.find_element_by_class_name("login-button")
+        self.login.submit()
+
+        self.driver.implicitly_wait(2)
+        self.invalidmessage = self.driver.find_element_by_tag_name('p')
+        assert self.invalidmessage.text == 'Your login attempt was not successful.'
+        if self.invalidmessage.text == u'Your login attempt was not successful.':
+            print ("Verify invalid login:")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+unittest.main()
